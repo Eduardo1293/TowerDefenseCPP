41b0804c5c8e2c739701263a35fec60fd183a391
Eduardo1293 <stefan.reso@gmx.de>
2018-08-13 20:25:06 +0200

    Windowsfrom Hinzugefügt

---
+++ TowerDefense/TowerDefense/AppStart.cpp
@@ -0,0 +1,11 @@
+#include "AppStart.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	TowerDefense::AppStart form;
+	Application::Run(%form);
+}
