8dbf1c0b2f3940df4e1cdcd5607270ec0e359acf
k3ule <andre.je@gmx.de>
2018-08-25 00:02:17 +0200

    Testtürme platziert, Testgegner mit hardgecodeter bewegung erstellt

--- TowerDefense/TowerDefense/AppStart.cpp
+++ TowerDefense/TowerDefense/AppStart.cpp
@@ -18,6 +18,8 @@ void Main(array<String^>^ args) {
 
 	//vertical sync gegen screen tearing
 	window.setVerticalSyncEnabled(true);
+	int x = 43;
+	int y = 0;
 
 	// run the program as long as the window is open
 	while (window.isOpen())
@@ -37,11 +39,63 @@ void Main(array<String^>^ args) {
 		// draw everything here...
 		// window.draw(...);
 		//Background
-		sf::Texture texture;
-		texture.loadFromFile("background.png");
-		sf::Sprite sprite;
-		sprite.setTexture(texture);
-		window.draw(sprite);
+		sf::Texture backgroundTexture;
+		backgroundTexture.loadFromFile("ArtAssets/background.png");
+		sf::Sprite backgroundSprite;
+		backgroundSprite.setTexture(backgroundTexture);
+		window.draw(backgroundSprite);
+
+		//Testturm und Testgegner, testgegner ist hardgecodet, dass er einen bestimmten weg abfährt
+		sf::Texture testTurmTexture;
+		testTurmTexture.loadFromFile("ArtAssets/tank_dark.png");
+		sf::Sprite testTurmSprite;
+		sf::Sprite testEnemySprite;
+		testEnemySprite.setTexture(testTurmTexture);
+		testEnemySprite.scale(0.5f, 0.5f);
+		//testgegner bewegungskram
+		if (y > 109 && y < 151 && x < 424) {
+			testEnemySprite.setRotation(270);
+		}
+		if (y <= 109) {
+			y++;
+		}
+		else if (y > 109 && x < 424) {
+			y = 150;
+			x++;
+		} 
+		else if (x = 424) {
+			testEnemySprite.setRotation(0);
+			y = 110;
+			x++;
+		}
+		if (x > 400) {
+			y++;
+		}
+		testEnemySprite.setPosition(x, y);
+		window.draw(testEnemySprite);
+
+		//testtürme statisch
+		testTurmSprite.scale(0.5f, 0.5f);
+		testTurmSprite.setTexture(testTurmTexture);
+		testTurmSprite.setPosition(43, 169);
+		window.draw(testTurmSprite);
+		testTurmSprite.setPosition(107, 169);
+		window.draw(testTurmSprite);
+		testTurmSprite.setPosition(171, 169);
+		window.draw(testTurmSprite);
+		testTurmSprite.setPosition(235, 169);
+		window.draw(testTurmSprite);
+		testTurmSprite.setPosition(299, 169);
+		window.draw(testTurmSprite);
+		testTurmSprite.setPosition(363, 169);
+		window.draw(testTurmSprite);
+		testTurmSprite.setPosition(235, 297);
+		window.draw(testTurmSprite);
+		testTurmSprite.setPosition(235, 425);
+		window.draw(testTurmSprite);
+		testTurmSprite.setPosition(235, 553);
+		window.draw(testTurmSprite);
+		//window.draw(testTurmSprite);
 		// end the current frame
 		window.display();
 	}
