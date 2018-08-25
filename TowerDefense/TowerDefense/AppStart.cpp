8b5211ef7d72a9c309105adf2e78473850150106
k3ule <andre.je@gmx.de>
2018-08-25 13:08:07 +0200

    aktueller Commit damit wir auf gleichem stand sind, kleine änderungen an den texturen

--- TowerDefense/TowerDefense/AppStart.cpp
+++ TowerDefense/TowerDefense/AppStart.cpp
@@ -14,12 +14,12 @@ void Main(array<String^>^ args) {
 
 	//SPIELFELD und WINDOW, das ist nur zu testzwecken hier
 	//Muss später in eine eigene Klasse, wenn das Menü funktioniert
-	sf::RenderWindow window(sf::VideoMode(512, 960), "Lennard is kacke");
+	sf::RenderWindow window(sf::VideoMode(512, 896), "Lennard is kacke.exe");
 
 	//vertical sync gegen screen tearing
 	window.setVerticalSyncEnabled(true);
 	int x = 43;
-	int y = 0;
+	int y = 64;
 
 	// run the program as long as the window is open
 	while (window.isOpen())
@@ -43,6 +43,7 @@ void Main(array<String^>^ args) {
 		backgroundTexture.loadFromFile("ArtAssets/background.png");
 		sf::Sprite backgroundSprite;
 		backgroundSprite.setTexture(backgroundTexture);
+		backgroundSprite.setPosition(0, 64);
 		window.draw(backgroundSprite);
 
 		//Testturm und Testgegner, testgegner ist hardgecodet, dass er einen bestimmten weg abfährt
@@ -51,21 +52,54 @@ void Main(array<String^>^ args) {
 		sf::Sprite testTurmSprite;
 		sf::Sprite testEnemySprite;
 		testEnemySprite.setTexture(testTurmTexture);
-		testEnemySprite.scale(0.5f, 0.5f);
+		testEnemySprite.scale(0.695f, 0.762f);
+
+		//test markierungen
+
+		sf::Texture emptySpaceTexture;
+		sf::Texture blockedSpaceTexture;
+		emptySpaceTexture.loadFromFile("ArtAssets/emptyspace.png");
+		blockedSpaceTexture.loadFromFile("ArtAssets/blockedspace.png");
+
+		sf::Sprite emptySpaceSprite;
+		sf::Sprite blockedSpaceSprite;
+		emptySpaceSprite.setTexture(emptySpaceTexture);
+		emptySpaceSprite.setColor(sf::Color(255, 255, 255, 128));
+		blockedSpaceSprite.setTexture(blockedSpaceTexture);
+		blockedSpaceSprite.setColor(sf::Color(255, 255, 255, 128));
+
+		emptySpaceSprite.setPosition(33, 224);
+		window.draw(emptySpaceSprite);
+
+		emptySpaceSprite.setPosition(97, 224);
+		window.draw(emptySpaceSprite);
+
+		emptySpaceSprite.setPosition(161, 224);
+		window.draw(emptySpaceSprite);
+
+		blockedSpaceSprite.setPosition(33, 160);
+		window.draw(blockedSpaceSprite);
+
+		blockedSpaceSprite.setPosition(97, 160);
+		window.draw(blockedSpaceSprite);
+
+		blockedSpaceSprite.setPosition(161, 160);
+		window.draw(blockedSpaceSprite);
+
 		//testgegner bewegungskram
-		if (y > 109 && y < 151 && x < 424) {
+		if (y > 173 && y < 215 && x < 424) {
 			testEnemySprite.setRotation(270);
 		}
-		if (y <= 109) {
+		if (y <= 173) {
 			y++;
 		}
-		else if (y > 109 && x < 424) {
-			y = 150;
+		else if (y > 173 && x < 424) {
+			y = 214;
 			x++;
 		} 
 		else if (x = 424) {
 			testEnemySprite.setRotation(0);
-			y = 110;
+			y = 174;
 			x++;
 		}
 		if (x > 400) {
@@ -75,25 +109,25 @@ void Main(array<String^>^ args) {
 		window.draw(testEnemySprite);
 
 		//testtürme statisch
-		testTurmSprite.scale(0.5f, 0.5f);
+		testTurmSprite.scale(0.695f, 0.762f);
 		testTurmSprite.setTexture(testTurmTexture);
-		testTurmSprite.setPosition(43, 169);
+		testTurmSprite.setPosition(43, 233);
 		window.draw(testTurmSprite);
-		testTurmSprite.setPosition(107, 169);
+		testTurmSprite.setPosition(107, 233);
 		window.draw(testTurmSprite);
-		testTurmSprite.setPosition(171, 169);
+		testTurmSprite.setPosition(171, 233);
 		window.draw(testTurmSprite);
-		testTurmSprite.setPosition(235, 169);
+		testTurmSprite.setPosition(235, 233);
 		window.draw(testTurmSprite);
-		testTurmSprite.setPosition(299, 169);
+		testTurmSprite.setPosition(299, 233);
 		window.draw(testTurmSprite);
-		testTurmSprite.setPosition(363, 169);
+		testTurmSprite.setPosition(363, 233);
 		window.draw(testTurmSprite);
-		testTurmSprite.setPosition(235, 297);
+		testTurmSprite.setPosition(235, 361);
 		window.draw(testTurmSprite);
-		testTurmSprite.setPosition(235, 425);
+		testTurmSprite.setPosition(235, 489);
 		window.draw(testTurmSprite);
-		testTurmSprite.setPosition(235, 553);
+		testTurmSprite.setPosition(235, 617);
 		window.draw(testTurmSprite);
 		//window.draw(testTurmSprite);
 		// end the current frame
