488274c6f4e28c7b15c6959a46c1eede44ecc05b
Eduardo1293 <stefan.reso@gmx.de>
2018-09-30 12:24:59 +0200

    Die Menü Buttons Hovern wieder

--- TowerDefense/TowerDefense/Menu.cpp
+++ TowerDefense/TowerDefense/Menu.cpp
@@ -22,14 +22,15 @@ void Menu::Run()
 	sf::Sprite menuBackgroundSprite;
 
 	sf::Color color;
+	sf::Color hoverColer = sf::Color(255, 255, 255, 140);
 
-	BasicButton StartButton = BasicButton(50, 25, "START", TexturePath, color.White, 38,0,0);
+	BasicButton StartButton = BasicButton(50, 25, "START", TexturePath, color.White, 38, 0, 0);
 	BasicButton BestenlisteButton = BasicButton(50, 120, "BESTENLISTE", TexturePath, color.White, 38, 45, 95);
-	BasicButton CreditsButton = BasicButton(50, 215, "CREDITS", TexturePath, color.White, 38,10,190);
-	BasicButton ExitButton = BasicButton(50, 310, "EXIT", TexturePath, color.White, 38, -20,285);
+	BasicButton CreditsButton = BasicButton(50, 215, "CREDITS", TexturePath, color.White, 38, 10, 190);
+	BasicButton ExitButton = BasicButton(50, 310, "EXIT", TexturePath, color.White, 38, -20, 285);
 	BasicButton ZurückButton = BasicButton(50, 320, "BACK", TexturePath, color.White, 38, -10, 295);
 
-	
+
 
 	LoadMenuTextures(menuBackgroundTexture, menuBackgroundSprite);
 
@@ -42,83 +43,113 @@ void Menu::Run()
 	bool showCredits = false;
 
 	MenuWindow.setFramerateLimit(24);
-
 	
 	while (MenuWindow.isOpen())
 	{
-		
+
 		sf::Event event;
-		
+
 
 		while (MenuWindow.pollEvent(event))
 		{
 			switch (event.type)
 			{
 
-			case sf::Event::Closed :
+			case sf::Event::Closed:
 				MenuWindow.close();
 				running = false;
-				break;				
+				break;
 			}
-				
+
 		}
 
 		sf::Vector2i localPosition = sf::Mouse::getPosition(MenuWindow);
 		sf::Vector2f mousePosF(static_cast<float>(localPosition.x), static_cast<float>(localPosition.y));
-		
+
 		MenuWindow.clear(sf::Color::Black);
-		
+
 		MenuWindow.draw(menuBackgroundSprite);
 
 		if (!showCredits)
 		{
 			menuBackgroundSprite.setScale(0.1, 0.1);
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 			{
 				if (StartButton.getSprite().getGlobalBounds().contains(mousePosF))
 				{
-					MenuWindow.close();
-					Game game;
-					while (game.isRunning())
-					{
-						game.Run();
+					StartButton.setColor(hoverColer);
 
+					if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+					{
+						MenuWindow.close();
+						Game game;
+						while (game.isRunning())
+						{
+							game.Run();
+
+						}
+						running = false;
 					}
-					running = false;
 				}
+				else
+				{
+					StartButton.setColor(color.White);
+				}
+
 				if (BestenlisteButton.getSprite().getGlobalBounds().contains(mousePosF))
 				{
+					BestenlisteButton.setColor(hoverColer);
 					//Öffnet die Bestenliste
+				}
+				else
+				{
+					BestenlisteButton.setColor(color.White);
 				}
 				if (CreditsButton.getSprite().getGlobalBounds().contains(mousePosF))
 				{
-					MenuWindow.clear(sf::Color::Black);
-					showButtons = false;
-					showCredits = true;
+					CreditsButton.setColor(hoverColer);
+					if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+					{
+						MenuWindow.clear(sf::Color::Black);
+						showButtons = false;
+						showCredits = true;
+					}
 
+				}
+				else
+				{
+					CreditsButton.setColor(color.White);
 				}
 				if (ExitButton.getSprite().getGlobalBounds().contains(mousePosF))
 				{
-					MenuWindow.close();
-					running = false;
+					ExitButton.setColor(hoverColer);
+					if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && !showCredits)
+					{
+						MenuWindow.close();
+						running = false;
+					}
 				}
-			}
-			MenuWindow.draw(StartButton.getSprite());
-			MenuWindow.draw(BestenlisteButton.getSprite());
-			MenuWindow.draw(CreditsButton.getSprite());
-			MenuWindow.draw(ExitButton.getSprite());
+				else
+				{
+					ExitButton.setColor(color.White);
+				}
 
-			MenuWindow.draw(StartButton.getButtonText());
-			MenuWindow.draw(BestenlisteButton.getButtonText());
-			MenuWindow.draw(CreditsButton.getButtonText());
-			MenuWindow.draw(ExitButton.getButtonText());
+				MenuWindow.draw(StartButton.getSprite());
+				MenuWindow.draw(BestenlisteButton.getSprite());
+				MenuWindow.draw(CreditsButton.getSprite());
+				MenuWindow.draw(ExitButton.getSprite());
+
+				MenuWindow.draw(StartButton.getButtonText());
+				MenuWindow.draw(BestenlisteButton.getButtonText());
+				MenuWindow.draw(CreditsButton.getButtonText());
+				MenuWindow.draw(ExitButton.getButtonText());
+			}
 		}
 		else
 		{
 			menuBackgroundSprite.setScale(1, 1);
 			menuBackgroundTexture.loadFromFile("ArtAssets/Menu/Credits.png");
 			menuBackgroundSprite.setTexture(menuBackgroundTexture);
-			MenuWindow.draw(ZurückButton.getSprite()); 
+			MenuWindow.draw(ZurückButton.getSprite());
 			MenuWindow.draw(ZurückButton.getButtonText());
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 			{
@@ -132,20 +163,20 @@ void Menu::Run()
 
 
 
+		}
 	}
-}
 
 
-void Menu::LoadMenuTextures(sf::Texture &menuBackgroundTexture,	sf::Sprite &menuBackgroundSprite)
-{
-	menuBackgroundTexture.loadFromFile("ArtAssets/Menu/Nebula Blue.png");
-	menuBackgroundSprite.setTexture(menuBackgroundTexture);
-	menuBackgroundSprite.setPosition(0, 0);
-	menuBackgroundSprite.setScale(0.1, 0.1);
-}
+	void Menu::LoadMenuTextures(sf::Texture &menuBackgroundTexture, sf::Sprite &menuBackgroundSprite)
+	{
+		menuBackgroundTexture.loadFromFile("ArtAssets/Menu/Nebula Blue.png");
+		menuBackgroundSprite.setTexture(menuBackgroundTexture);
+		menuBackgroundSprite.setPosition(0, 0);
+		menuBackgroundSprite.setScale(0.1, 0.1);
+	}
 
 
-bool Menu::isRunning()
-{
-	return running;
-}
+	bool Menu::isRunning()
+	{
+		return running;
+	}
